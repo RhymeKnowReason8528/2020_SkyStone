b12115385c304b2aa8945430fdf280e4d6c7254c
Willilio <32961117+Willilio@users.noreply.github.com>
2020-01-10 12:25:25 -0500

    Updated West Coast (One Controller)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -0,0 +1,250 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.Locale;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="RKR Autonomous")
+public class Autonomous extends LinearOpMode {
+
+    /*private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
+    private static final String VUFORIA_KEY = "ARjoxi3/////AAABmXAUt9n1wEjDvzITTR3QybQ8V/6WWaLzzVteSXjxrJxxyilUnEB7VpV53J57ifvzM7/fbFhFTg7GNDdZs+QVVoZQ7Pt0SjP9THhkkR9zj42ztarG+IUc8Id5pW1juuKzHyiKz4sJWmDI6I3RVsa9R/H4rWUcdQUvWBF7X/lTOSMOHHPuz09pIDtLU/BTpL47hjCefksIY4VJ0KurxkrwcvbHufGh50i7j5jW74dt/NJrG2NPkt134vmeyzLI3/cBNIsOOZrWetxzQ0cXPAkS+3MItc53kFV+RTYt+C2t2xlaydfbAvboHFXExl+nHzh5AA+12JbvK3ikBBOnmORkkoDD0ncscgD6lKeEkQvOl0zw";
+    private VuforiaLocalizer vuforia = null;
+    private boolean targetVisible = false;
+
+    List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();*/
+
+    // The IMU sensor object
+    BNO055IMU imu;
+
+    // State used for updating telemetry
+    Orientation angles;
+    Acceleration gravity;
+
+    private DcMotor LeftMotor;
+    private DcMotor RightMotor;
+
+    private DcMotor LeftIntakeMotor;
+    private DcMotor RightIntakeMotor;
+
+    private ColorSensor BlockSensor;
+
+    static final double     COUNTS_PER_MOTOR_REV   = 1440 ;
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    public Autonomous() {
+        /*VuforiaTrackables targetsSkyStone = this.vuforia.loadTrackablesFromAsset("Skystone");
+
+        VuforiaTrackable stoneTarget = targetsSkyStone.get(0);
+        stoneTarget.setName("Stone Target");
+        VuforiaTrackable blueRearBridge = targetsSkyStone.get(1);
+        blueRearBridge.setName("Blue Rear Bridge");
+        VuforiaTrackable redRearBridge = targetsSkyStone.get(2);
+        redRearBridge.setName("Red Rear Bridge");
+        VuforiaTrackable redFrontBridge = targetsSkyStone.get(3);
+        redFrontBridge.setName("Red Front Bridge");
+        VuforiaTrackable blueFrontBridge = targetsSkyStone.get(4);
+        blueFrontBridge.setName("Blue Front Bridge");
+        VuforiaTrackable red1 = targetsSkyStone.get(5);
+        red1.setName("Red Perimeter 1");
+        VuforiaTrackable red2 = targetsSkyStone.get(6);
+        red2.setName("Red Perimeter 2");
+        VuforiaTrackable front1 = targetsSkyStone.get(7);
+        front1.setName("Front Perimeter 1");
+        VuforiaTrackable front2 = targetsSkyStone.get(8);
+        front2.setName("Front Perimeter 2");
+        VuforiaTrackable blue1 = targetsSkyStone.get(9);
+        blue1.setName("Blue Perimeter 1");
+        VuforiaTrackable blue2 = targetsSkyStone.get(10);
+        blue2.setName("Blue Perimeter 2");
+        VuforiaTrackable rear1 = targetsSkyStone.get(11);
+        rear1.setName("Rear Perimeter 1");
+        VuforiaTrackable rear2 = targetsSkyStone.get(12);
+        rear2.setName("Rear Perimeter 2");
+
+        // For convenience, gather together all the trackable objects in one easily-iterable collection
+        allTrackables.addAll(targetsSkyStone);*/
+    }
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+
+    double getHeading() {
+        return AngleUnit.DEGREES.normalize(AngleUnit.DEGREES.fromUnit(angles.angleUnit, angles.firstAngle));
+    }
+
+    void setIntakePower(double power) {
+        LeftIntakeMotor.setPower(power);
+        RightIntakeMotor.setPower(power);
+    }
+
+    public void doTelemetry() {
+        telemetry.addData("Heading: ", formatAngle(angles.angleUnit, angles.firstAngle));
+        telemetry.addData("Roll: ", formatAngle(angles.angleUnit, angles.secondAngle));
+        telemetry.addData("Pitch: ", formatAngle(angles.angleUnit, angles.thirdAngle));
+        telemetry.addData("Sensed Block: ", checkForBlock());
+
+        /*targetVisible = false;
+        for (VuforiaTrackable trackable : allTrackables) {
+            if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
+                telemetry.addData("Visible Target: ", trackable.getName());
+                targetVisible = true;
+            }
+        } if (!targetVisible) {
+            telemetry.addData("Visible Target: ", "None");
+        }*/
+
+        telemetry.update();
+    }
+
+    public void resetEncoders() {
+        LeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        LeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void autoTurn(double degrees, double power) {
+        double startHeading = getHeading();
+        double endHeading = startHeading+degrees;
+        if (endHeading < -180) {endHeading += 360;}
+        if (endHeading > 180) {endHeading -= 360;}
+
+        if (degrees < 0) {
+            LeftMotor.setPower(power);
+            RightMotor.setPower(-power);
+            while (getHeading() > endHeading && opModeIsActive()) {
+                angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                gravity  = imu.getGravity();
+                doTelemetry();
+            }
+            LeftMotor.setPower(0);
+            RightMotor.setPower(0);
+        } else {
+            LeftMotor.setPower(-power);
+            RightMotor.setPower(power);
+            while (getHeading() < endHeading && opModeIsActive()) {
+                angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                gravity  = imu.getGravity();
+                doTelemetry();
+            }
+            LeftMotor.setPower(0);
+            RightMotor.setPower(0);
+        }
+    }
+
+    public void autoDrive(double inches, double power, double times) {
+        ElapsedTime timer = new ElapsedTime();
+
+        resetEncoders();
+
+        int newLeftTarget = LeftMotor.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH);
+        int newRightTarget = RightMotor.getCurrentPosition() + (int)(inches * COUNTS_PER_INCH);
+        LeftMotor.setTargetPosition(newLeftTarget);
+        RightMotor.setTargetPosition(newRightTarget);
+
+        LeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        LeftMotor.setPower(Math.abs(power));
+        RightMotor.setPower(Math.abs(power));
+
+        timer.reset();
+        double startTime = timer.seconds();
+        while (opModeIsActive() && (LeftMotor.isBusy() && RightMotor.isBusy()) && timer.seconds()<times) {
+            telemetry.addData("Targets: ",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+            telemetry.addData("Current Pos: ",  "Running at %7d :%7d",
+                    LeftMotor.getCurrentPosition(),
+                    RightMotor.getCurrentPosition());
+        }
+
+        LeftMotor.setPower(0);
+        RightMotor.setPower(0);
+        LeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    private boolean checkForBlock() {
+        int b = BlockSensor.blue(); int r = BlockSensor.red(); int g = BlockSensor.green();
+        return r > b && g > b && r + g > 100;
+    }
+
+    private void changeCSLightStatus(boolean enable) {
+        BlockSensor.enableLed(enable);
+    }
+
+    @Override
+    public void runOpMode() {
+        // Drive motor configuration
+        LeftMotor = hardwareMap.get(DcMotor.class, "left_drive");
+        RightMotor = hardwareMap.get(DcMotor.class, "right_drive");
+
+        LeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        RightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Intake motor configuration
+        LeftIntakeMotor = hardwareMap.get(DcMotor.class, "left_intake");
+        RightIntakeMotor = hardwareMap.get(DcMotor.class, "right_intake");
+
+        BlockSensor = hardwareMap.get(ColorSensor.class, "block_sensor");
+
+        LeftIntakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RightIntakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        //Vuforia
+
+        //int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        //VuforiaLocalizer.Parameters vuparameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        //vuparameters.vuforiaLicenseKey = VUFORIA_KEY;
+        //vuparameters.cameraDirection   = CAMERA_CHOICE;
+
+        //vuforia = ClassFactory.getInstance().createVuforia(vuparameters);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            gravity = imu.getGravity();
+
+            changeCSLightStatus(true);
+
+            autoTurn(70, 0.4);
+            autoDrive(12, 0.4, 0.5);
+
+            changeCSLightStatus(false);
+
+            break;
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoast.java
@@ -0,0 +1,124 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="West Coast")
+public class WestCoast extends OpMode {
+    private double LeftMotorPower;
+    private double RightMotorPower;
+
+    private double IntakePower;
+
+    private double LinearSlidePower;
+
+    private DcMotor LeftMotor;
+    private DcMotor RightMotor;
+
+    private DcMotor LeftIntakeMotor;
+    private DcMotor RightIntakeMotor;
+
+    private DcMotor LinearSlideMotor;
+
+    private final double MaxPower = 1;
+
+    private final double MaxDrivePower = Math.min(0.7, MaxPower);
+    private final double MaxIntakePower = Math.min(0.7, MaxPower);
+
+    private final double LinearSlideMotorMaxPower = Math.min(0.6, MaxPower);
+
+    private ElapsedTime runtime;
+
+    private final String LeftMotorName = "left_drive";
+    private final String RightMotorName = "right_drive";
+    private final String LeftIntakeName = "left_intake";
+    private final String RightIntakeName = "right_intake";
+    private final String SlideMotorName = "slide_motor";
+
+    @Override
+    public void init() {
+        // Drive motor configuration
+        LeftMotor = hardwareMap.get(DcMotor.class, LeftMotorName);
+        RightMotor = hardwareMap.get(DcMotor.class, RightMotorName);
+
+        LeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        LeftMotorPower = 0;
+        RightMotorPower = 0;
+
+        // Intake motor configuration
+        LeftIntakeMotor = hardwareMap.get(DcMotor.class, LeftIntakeName);
+        RightIntakeMotor = hardwareMap.get(DcMotor.class, RightIntakeName);
+
+        LeftIntakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RightIntakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        IntakePower = 0;
+
+        // Slide motor configuration
+        LinearSlideMotor = hardwareMap.get(DcMotor.class, SlideMotorName);
+
+        LinearSlideMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        LinearSlidePower = 0;
+
+        // Timer setup
+        runtime = new ElapsedTime();
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    @Override
+    public void loop() {
+        // Motor control calculations
+        LeftMotorPower = Math.min(MaxDrivePower, Math.max(gamepad1.left_stick_y, -MaxDrivePower));
+        RightMotorPower = Math.min(MaxDrivePower, Math.max(gamepad1.right_stick_y, -MaxDrivePower));
+
+        LeftMotorPower /= gamepad1.left_trigger+1;
+        RightMotorPower /= gamepad1.right_trigger+1;
+
+        // Gripper reading
+        IntakePower = Math.min(MaxIntakePower, gamepad2.right_trigger);
+        IntakePower -= Math.min(MaxIntakePower, gamepad2.left_trigger);
+
+        // Motor power
+        LeftMotor.setPower(LeftMotorPower);
+        RightMotor.setPower(RightMotorPower);
+
+        LeftIntakeMotor.setPower(IntakePower);
+        RightIntakeMotor.setPower(IntakePower);
+
+        // Linear slide power
+        if (gamepad2.dpad_up) {
+            LinearSlidePower = LinearSlideMotorMaxPower;
+        } else if (gamepad2.dpad_down) {
+            LinearSlidePower = -LinearSlideMotorMaxPower;
+        } else {
+            LinearSlidePower = 0;
+        } LinearSlideMotor.setPower(LinearSlidePower);
+
+        // Telemetry
+        telemetry.addData("Runtime: ", runtime);
+        telemetry.addLine();
+        telemetry.addData("Left Power: ", LeftMotorPower);
+        telemetry.addData("Right Power: ", RightMotorPower);
+        telemetry.addLine();
+        telemetry.addData("Intake Power: ", IntakePower);
+        telemetry.addLine();
+        telemetry.addData("Linear Slide Power: ", LinearSlidePower);
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+        LeftMotor.setPower(0);
+        RightMotor.setPower(0);
+    }
+}
