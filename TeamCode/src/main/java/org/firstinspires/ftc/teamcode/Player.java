27d06927987ded5cda1055557adecfc006c5c572
Willilio <32961117+Willilio@users.noreply.github.com>
2020-02-16 09:23:25 -0500

    Worked on player

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Player.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Player.java
@@ -1,4 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Player {
+import android.media.MediaPlayer;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.HashMap;
+
+class Player {
+    boolean MusicPaused;
+
+    private HardwareMap hardwareMap;
+
+    private HashMap<String, Integer> Songs;
+
+    private MediaPlayer MP;
+
+    Player(HardwareMap hw) {
+        Songs = new HashMap<>();
+        Songs.put("ucanttouchthis",R.raw.ucanttouchthis);
+        Songs.put("megalovania",R.raw.megalovania);
+        Songs.put("coconutmall",R.raw.coconutmall);
+
+        hardwareMap = hw;
+
+        MusicPaused = true;
+    }
+
+    void setSong(String name) {
+        MP = MediaPlayer.create(hardwareMap.appContext,Songs.get(name));
+    }
+
+    void start() {
+        MP.start();
+        MusicPaused = false;
+    }
+
+    void stop() {
+        MP.stop();
+        MusicPaused = true;
+    }
 }
